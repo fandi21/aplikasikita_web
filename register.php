67deb4219756355a3a833e57f2b4398164c7daf4
fandi21 <fandi.smart21@gmail.com>
2023-06-10 14:17:30 +0700

    commit
    Signed-off-by: fandi21 <fandi.smart21@gmail.com>

--- register.php
+++ register.php
@@ -1,30 +1,30 @@
 <?php
     require 'init.php';
 
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
-    $name = $_POST['name'];
+    $name = $_GET['name'];
+    $user_name = $_GET['user_name'];
+    $password = $_GET['password']; 
 
     if($con){
         $sql = "SELECT * FROM user_info WHERE user_name='$user_name'";
         $result = mysqli_query($con, $sql);
         if(mysqli_num_rows($result) > 0){
-            $status = "OK";
+            $status = "Data Exist";
             $result_code = 0;
             echo json_encode(array('status'=>$status, 'result_code'=>$result_code));
         }else{
-            $sql = "INSERT INTO user_info VALUES ('$name', '$user_name', '$password')";
+            $sql = "INSERT INTO user_info (name,user_name,password) VALUES ('$name', '$user_name', '$password')";
             if(mysqli_query($con, $sql)){
                 $status = "OK";
                 $result_code = 1;
                 echo json_encode(array('status'=>$status, 'result_code'=>$result_code));
             }else{
-                $status = "Failed";
+                $status = "Failed insert";
                 echo json_encode(array('status'=>$status), JSON_FORCE_OBJECT);
             }
         }
     }else{
-        $status = "Failed";
+        $status = "Failed connect";
         echo json_encode(array('status'=>$status), JSON_FORCE_OBJECT);
     }
 ?>
